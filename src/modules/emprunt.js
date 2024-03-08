282d5a777fb62518fddb3d174f8d705141c53cc5
SukiTsu <buibrandon47@gmail.com>
2024-03-08 11:33:09 +0100

    Modification de la page de calcul
    Modification de la méthode de calcule et ajout d'une vérification des données

--- src/modules/emprunt.js
+++ src/modules/emprunt.js
@@ -1,17 +1,8 @@
 function calculEmprunt(capital, taux, temps){
-    temps *= 12
-
-    let taux_mensuel = Math.pow((1 + taux/100),1/12) - 1;
-    //console.log("Taux mensuel: ", taux_mensuel);
-
-    let c1 = capital * taux_mensuel * Math.pow(1+taux_mensuel,temps);
-    let c2 = c1 / (Math.pow(1+taux_mensuel, temps) - 1);
-    //console.log("Montant mensualite: ",c2);
-
-    let interet = c2*temps - capital;
-    //console.log("intere: ", interet);
-
-    let mensualite = [capital * (taux/12)]/[1 - (1 + (taux/12) - (12 * temps))]
+    temps *= 12;
+    taux /= 100;
+    let c1 = capital * (taux/12);
+    let mensualite = c1/ (1 - (1 / Math.pow(1 + (taux/12),temps)) )
     //console.log("mensual", mensualite);
 
     return mensualite
--- src/routes/formEmprunt.js
+++ src/routes/formEmprunt.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const emprunt = require("./../modules/emprunt")
+const e = require("express");
 
 const tabResultCalculEmprunts = [];
 
@@ -11,7 +12,7 @@ router.get('/formEmprunt', (req, res, next) => {
     if (!req.session.isLogin)
         res.redirect("/")
     else
-        res.render(path.join(__dirname,"..","views","formCalculEmprunt.ejs"),{pageTitle: "Formulaire calcul emprunt", isUserLogin: true});
+        res.render(path.join(__dirname,"..","views","formCalculEmprunt.ejs"),{pageTitle: "Formulaire calcul emprunt", isUserLogin: true, errorMessage: null});
 
 });
 
@@ -23,13 +24,19 @@ router.post('/formEmprunt', (req, res, next) => {
     let taux = req.body.taux
     let duree = req.body.duree
 
-    //on calcule les mensualités de l'emprunt
-    let mensualite = emprunt(capital, taux, duree)
+    let erreur = "";
+    if (!isNaN(capital) && !isNaN(taux) && !isNaN(duree)) {
+        //on calcule les mensualités de l'emprunt
+        let mensualite = emprunt(capital, taux, duree)
+        //on stocke les données de calcul et le résultat dans le tableau
+        tabResultCalculEmprunts.push({capital: capital, taux: taux, duree: duree, mensualite: mensualite})
+        //console.log(tabResultCalculEmprunts);
+        res.redirect('/resultEmprunt');
+    }else{
+        erreur = "Veuillez entré que des nombres";
+        res.render(path.join(__dirname,"..","views","formCalculEmprunt.ejs"),{pageTitle: "Formulaire calcul emprunt", isUserLogin: true, errorMessage: erreur});
 
-    //on stocke les données de calcul et le résultat dans le tableau
-    tabResultCalculEmprunts.push({capital: capital, taux: taux, duree: duree, mensualite: mensualite})
-    //console.log(tabResultCalculEmprunts);
-    res.redirect('/resultEmprunt');
+    }
 });
 
 module.exports = router;
