b9251167f64cd69a1e667ab678b705b6eb4a592b
DurandAntonin <antonin.durand576@gmail.com>
2024-03-08 08:25:16 +0100

    Init de l'application

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require("express-session")
+
+const port=8000;
+const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(session({secret : "un secret", resave: false, saveUninitialized: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use((req,res,next)=>{;
+    //res.status(404).render(path.join(__dirname,"views","404.ejs"), {pageTitle: "404 not Found"});
+});
+
+app.listen(port);
+console.log("serveur http à l'écoute sur le port : "+port);
